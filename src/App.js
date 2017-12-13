d3dab847f09782262427ba7829e7b28e11b2f256
yomoda07 <kanji.yy@gmail.com>
2017-12-13 11:57:24 -0500

    Install and set up firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import './App.css';
 import RegisterEnglishForm from './components/RegisterEnglishForm';
 
+const config = {
+    apiKey: "AIzaSyBrmEDw2473a4aG53WSura1XJQpOh-eKoE",
+    authDomain: "snap-up.firebaseapp.com",
+    databaseURL: "https://snap-up.firebaseio.com",
+    projectId: "snap-up",
+    storageBucket: "snap-up.appspot.com",
+    messagingSenderId: "754203017870"
+  };
+
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp(config);
+  }
 
   render() {
     return (
